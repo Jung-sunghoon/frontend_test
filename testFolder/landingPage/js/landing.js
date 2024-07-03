5f6013afd1e64030657de73fc00124184ce4d2b3
JShoon <jsh021813@gmail.com>
2024-07-03 17:33:40 +0900

    feat: 캐러셀, 햄버거 메뉴 구현

--- testFolder/landingPage/js/landing.js
+++ testFolder/landingPage/js/landing.js
@@ -6,10 +6,16 @@ const email = document.getElementById("email");
 const form = document.querySelector(".inputWrapper");
 const topBtn = document.querySelector(".topBtn");
 const container = document.querySelector(".imgSection2ImgContainer");
-const images = document.querySelector(".catImg");
+const imagesWrapper = document.querySelector(".imgSection2ImgWrapper");
+const images = document.querySelectorAll(".catImg");
 const prevBtn = document.querySelector(".carouselBtnLeft");
 const nextBtn = document.querySelector(".carouselBtnRight");
+const menuImg = document.querySelector(".mobileHeaderMenuImg");
+const closeMenuImg = document.querySelector(".closeMenuImg");
+const mobileMenuContainer = document.querySelector(".mobileMenuContainer");
+const mobileMenu = document.querySelector(".mobileHeaderMenu");
 let currentIndex = 0;
+const slideWidth = images[0].clientWidth;
 
 form.addEventListener("submit", function (event) {
   event.preventDefault(); // 기본 폼 제출 막기
@@ -40,4 +46,53 @@ topBtn.addEventListener("click", function () {
     top: 0,
     behavior: "smooth",
   });
+});
+
+menuImg.addEventListener("click", () => {
+  mobileMenuContainer.classList.add("open");
+  mobileMenu.classList.add("open");
+});
+
+closeMenuImg.addEventListener("click", () => {
+  mobileMenuContainer.classList.remove("open");
+  mobileMenu.classList.remove("open");
+});
+
+// 오른쪽 화살표 클릭 시
+nextBtn.addEventListener("click", () => {
+  currentIndex++;
+  if (currentIndex > images.length - 1) {
+    currentIndex = 0;
+  }
+  slideImages();
+});
+
+// 왼쪽 화살표 클릭 시
+prevBtn.addEventListener("click", () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  slideImages();
+});
+
+function slideImages() {
+  const offset = -currentIndex * slideWidth;
+  imagesWrapper.style.transform = `translateX(${offset}px)`;
+}
+
+// 화면 로드 후 첫 번째 이미지 표시
+slideImages();
+
+document.addEventListener("click", (event) => {
+  // 클릭된 요소가 모바일 메뉴 토글 이미지 자신이거나, 모바일 메뉴 컨테이너 내부 요소이면 무시
+  if (event.target === menuImg || mobileMenu.contains(event.target)) {
+    return;
+  }
+
+  // 모바일 메뉴가 열려있으면 닫기
+  if (mobileMenuContainer.classList.contains("open")) {
+    mobileMenuContainer.classList.remove("open");
+    mobileMenu.classList.remove("open");
+  }
 });
